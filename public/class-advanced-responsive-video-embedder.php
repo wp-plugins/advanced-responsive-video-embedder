46c41b4a8e9c11133826a78901c83f3c17f44173
nico23 <nico23@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-07 23:02:33 +0000

    release of next version
    git-svn-id: https://plugins.svn.wordpress.org/advanced-responsive-video-embedder/trunk@944593 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/class-advanced-responsive-video-embedder.php
+++ public/class-advanced-responsive-video-embedder.php
@@ -24,7 +24,7 @@ class Advanced_Responsive_Video_Embedder {
 	 * @since   2.6.0
 	 * @var     string
 	 */
-	const VERSION = '4.8.0';
+	const VERSION = '4.9.0';
 
 	/**
 	 * Unique identifier for your plugin.
