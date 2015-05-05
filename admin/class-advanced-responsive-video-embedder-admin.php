c18c08669146290d6a4709fb11c28eeb55cf2f3c
nico23 <nico23@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 18:35:44 +0000

    release of next version
    git-svn-id: https://plugins.svn.wordpress.org/advanced-responsive-video-embedder/trunk@1154068 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/class-advanced-responsive-video-embedder-admin.php
+++ admin/class-advanced-responsive-video-embedder-admin.php
@@ -315,6 +315,23 @@ class Advanced_Responsive_Video_Embedder_Admin {
 		);
 	}
 	
+	public function admin_notice_2() {
+		
+		global $current_user ;
+		$user_id = $current_user->ID;	
+		
+		if ( get_user_meta( $user_id, 'arve_ignore_admin_notice_2' ) ) {
+			return;
+		} ?>
+		
+		<div class="updated">
+			<p>There will be a Pro Addon for Advanced Resonsive Video Embedder, I need your help to help me out testing. Please <a href="https://nextgenthemes.com/?p=1371">read this</a> you will get the Pro Addon for <strong>FREE!</strong> | <a href="?arve_msg_ignore=2">Dismiss</a>
+			</p>
+		</div>
+
+		<?php
+	}
+	
 	/**
 	 * Display a notice that can be dismissed
 	 *
@@ -355,6 +372,11 @@ class Advanced_Responsive_Video_Embedder_Admin {
 		//* If user clicks to ignore the notice, add that to their user meta
 		if ( isset( $_GET['arve_nag_ignore'] ) && '1' == $_GET['arve_nag_ignore'] ) {
 			add_user_meta( $user_id, 'arve_ignore_admin_notice', 'true', true );
+		}
+		
+		//* If user clicks to ignore the notice, add that to their user meta
+		if ( isset( $_GET['arve_nag_ignore'] ) && '2' == $_GET['arve_nag_ignore'] ) {
+			add_user_meta( $user_id, 'arve_ignore_admin_notice_2', 'true', true );
 		}
 	}
 
---
+++ readme/description-experimental-modes.md
@@ -0,0 +1,17 @@
+### Experimental Modes ###
+
+The pro version has two experimental modes.
+
+#### Lazyload -> Fullscreen ####
+
+This is sort of a dream come true because to me it never really made sense to watch videos in small boxes in the middle of the page, lightboxes are half way there but why not go straight to fullscreen? You now can. There is a issue though and that is the fact that technically this uses the HTML 5 fullscreen feature on the iframe with the video in it. This means if you click the fullscreen button from the Fullscreen it goes to fullscreen and 2nd time. When flash gets involved this can end up in some even more weird behavior.
+
+[youtube id="WHZPEkZCqwA" mode="lazyload-fullscreen"]
+
+[videojug id="fa15cafd-556f-165b-d660-ff0008c90d2d" mode="lazyload-fullscreen"]
+
+#### Lazyload -> Fixed ####
+
+This opens up the video in a risizable (making it smaller only works in Firefox) fixed element on the page with the video.
+
+[youtube id="WHZPEkZCqwA" mode="lazyload-fixed"]
